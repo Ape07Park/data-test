9d740c9c8d5c013681e49ed9aa301d2f76b7aab5
SUNGMIN PARK <ape072399@gmail.com>
2024-12-12 12:24:40 +0900

    스크롤, 검색, 정렬 개별로 구현 준비

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Main from './pages/Main';
 import TocList from './pages/TocList';
 import Scroll from './pages/Scroll';
+import Search from './pages/Search';
+import Sort from './pages/Sort';
 
 function App() {
-
 
   return (
     <BrowserRouter>
@@ -13,6 +14,9 @@ function App() {
         <Route path="/" element={<Main />} />
         <Route path="/list" element={<TocList />} />
         <Route path="/scroll" element={<Scroll />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/sort" element={<Sort />} />
+
       </Routes>
       {/* 푸터 */}
     </BrowserRouter>
